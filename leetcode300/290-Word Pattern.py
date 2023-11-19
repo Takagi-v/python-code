39464d212578281b31f9c38bd591e70cbb862258
ass-ass-ass <caoyilu1122@gmail.com>
2023-11-19 21:51:02 +0800

    #

--- leetcode300/290-Word Pattern.py
+++ leetcode300/290-Word Pattern.py
@@ -5,23 +5,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        p=s.split()
-        l=len(p)
-        if len(pattern)!=l:
+        p = s.split()
+        l = len(p)
+        if len(pattern) != l:
             return False
-        dt={}
-        st={}
+        dt = {}
+        st = {}
         for i in range(l):
             if p[i] in dt:
                 if pattern[i] not in st:
                     return False
-                if dt[p[i]]!=pattern[i]:
+                if dt[p[i]] != pattern[i]:
                     return False
             if pattern[i] in st:
                 if p[i] not in dt:
                     return False
-                if st[pattern[i]]!=p[i]:
+                if st[pattern[i]] != p[i]:
                     return False
-            dt[p[i]]=pattern[i]
-            st[pattern[i]]=p[i]
+            dt[p[i]] = pattern[i]
+            st[pattern[i]] = p[i]
         return True
